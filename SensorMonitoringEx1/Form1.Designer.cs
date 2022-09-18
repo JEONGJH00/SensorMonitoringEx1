a8fe320c833237f114b933a6f13fb0433d9ba44c
JEONGJAEHYUNG00 <jason523930@gmail.com>
2022-09-18 18:32:08 +0900

    초기 커밋.

---
+++ SensorMonitoringEx1/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace SensorMonitoringEx1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.lblConnectTime = new System.Windows.Forms.Label();
+            this.txtCount = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnDisConnect = new System.Windows.Forms.Button();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblConnectTime
+            // 
+            this.lblConnectTime.AutoSize = true;
+            this.lblConnectTime.Location = new System.Drawing.Point(12, 37);
+            this.lblConnectTime.Name = "lblConnectTime";
+            this.lblConnectTime.Size = new System.Drawing.Size(122, 15);
+            this.lblConnectTime.TabIndex = 0;
+            this.lblConnectTime.Text = "Connection Time:";
+            // 
+            // txtCount
+            // 
+            this.txtCount.Location = new System.Drawing.Point(326, 24);
+            this.txtCount.Name = "txtCount";
+            this.txtCount.Size = new System.Drawing.Size(100, 25);
+            this.txtCount.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnDisConnect);
+            this.groupBox1.Controls.Add(this.btnConnect);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Location = new System.Drawing.Point(438, 37);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(137, 112);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "port";
+            // 
+            // btnDisConnect
+            // 
+            this.btnDisConnect.Location = new System.Drawing.Point(7, 83);
+            this.btnDisConnect.Name = "btnDisConnect";
+            this.btnDisConnect.Size = new System.Drawing.Size(121, 23);
+            this.btnDisConnect.TabIndex = 2;
+            this.btnDisConnect.Text = "DisConnect";
+            this.btnDisConnect.UseVisualStyleBackColor = true;
+            this.btnDisConnect.Click += new System.EventHandler(this.btnDisConnect_Click);
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(7, 54);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(121, 23);
+            this.btnConnect.TabIndex = 1;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(7, 25);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 23);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(15, 62);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(411, 139);
+            this.listBox1.TabIndex = 4;
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(15, 219);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(560, 316);
+            this.chart1.TabIndex = 5;
+            this.chart1.Text = "chart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(592, 548);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.txtCount);
+            this.Controls.Add(this.lblConnectTime);
+            this.Name = "Form1";
+            this.Text = "photoCell";
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblConnectTime;
+        private System.Windows.Forms.TextBox txtCount;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnDisConnect;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    }
+}
+
---
+++ SensorMonitoringEx1/Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Data.SqlClient;
+
+namespace SensorMonitoringEx1
+{
+    public partial class Form1 : Form
+    {
+        private SerialPort sPort;
+        private int xCount = 200; //
+        List<SensorData> myData = new List<SensorData>();
+        SqlConnection conn;
+//        string connString = @"Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\정재형\\source\\repos\\SensorMonitoringEx1\\SensorMonitoringEx1\\SensorData.mdf;Integrated Security=True";
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Combobox
+            foreach(var port in SerialPort.GetPortNames())
+            {
+                comboBox1.Items.Add(port);
+            }
+            comboBox1.Text = "Select Port";
+
+            
+
+            // Chart 세팅
+            ChartSetting();
+
+            // txtCount
+            txtCount.TextAlign = HorizontalAlignment.Center;
+
+            // Connect, DistConnect 버튼
+            btnConnect.Enabled = false;
+            btnDisConnect.Enabled = false;
+        }
+
+        private void ChartSetting()
+        {
+            chart1.ChartAreas.Clear();
+            chart1.ChartAreas.Add("draw");
+            chart1.ChartAreas["draw"].AxisX.Minimum = 0;
+            chart1.ChartAreas["draw"].AxisX.Maximum = xCount;
+            chart1.ChartAreas["draw"].AxisX.Interval = xCount / 4;
+            chart1.ChartAreas["draw"].AxisX.MajorGrid.LineColor = Color.White;
+            chart1.ChartAreas["draw"].AxisX.MajorGrid.LineDashStyle = ChartDashStyle.Dash;
+
+            chart1.ChartAreas["draw"].AxisY.Minimum = 0;
+            chart1.ChartAreas["draw"].AxisY.Maximum = 120;
+            chart1.ChartAreas["draw"].AxisY.Interval = 10;
+            chart1.ChartAreas["draw"].AxisY.MajorGrid.LineColor = Color.White;
+            chart1.ChartAreas["draw"].AxisY.MajorGrid.LineDashStyle = ChartDashStyle.Dash;
+
+            chart1.ChartAreas["draw"].BackColor = Color.White;
+
+            chart1.ChartAreas["draw"].CursorX.AutoScroll = true;
+
+            chart1.ChartAreas["draw"].AxisX.ScaleView.Zoomable = true;
+            chart1.ChartAreas["draw"].AxisX.ScrollBar.ButtonStyle = ScrollBarButtonStyles.SmallScroll;
+            chart1.ChartAreas["draw"].AxisX.ScrollBar.ButtonColor = Color.LightSteelBlue;
+
+            chart1.Series.Clear();
+            chart1.Series.Add("PhotoCell");
+            chart1.Series["PhotoCell"].ChartType = SeriesChartType.Line;
+            chart1.Series["PhotoCell"].Color = Color.Black;
+            chart1.Series["PhotoCell"].BorderWidth = 6;
+            if (chart1.Legends.Count > 0)
+                chart1.Legends.RemoveAt(0);
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ComboBox cb = sender as ComboBox;
+            sPort = new SerialPort(cb.SelectedItem.ToString());
+            sPort.Open();
+            sPort.DataReceived += SPort_DataReceived;
+
+            lblConnectTime.Text = "Connection Time : " + DateTime.Now.ToString();
+            btnDisConnect.Enabled = true;
+        }
+
+        private void SPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            string s = sPort.ReadLine();
+            this.BeginInvoke(new Action(delegate { ShowValue(s); }));
+        }
+
+        private void ShowValue(string s)
+        {
+            int v = Int32.Parse(s);
+            try
+            {
+                v = Int32.Parse(s);
+            }
+            catch(FormatException)
+            {
+                Console.WriteLine("Unable to convert {0}", s);
+                return;
+            }
+            if (v < 0 || v > 120) // 센서값이 0보다 작거나 120보다 크면 버린다.
+                return;
+
+            SensorData data = new SensorData(
+                DateTime.Now.ToShortDateString(),
+                DateTime.Now.ToString("HH:mm:ss"),
+                v);
+
+            myData.Add(data);
+            //DBInsert(data);
+
+            txtCount.Text = myData.Count.ToString();
+            //progressBar1.Value = v;
+
+            string item = DateTime.Now.ToString() + "\t" + s;
+            listBox1.Items.Add(item);
+            listBox1.SelectedIndex = listBox1.Items.Count - 1; // 스크롤
+
+            // Chart
+            chart1.Series["PhotoCell"].Points.Add(v);
+
+            // 차트 스크롤 하는 부분
+            chart1.ChartAreas["draw"].AxisX.Minimum = 0;
+            chart1.ChartAreas["draw"].AxisX.Maximum =
+                (myData.Count >= 200) ? myData.Count : xCount;
+            if (myData.Count > xCount)
+                chart1.ChartAreas["draw"].AxisX.ScaleView.Zoom
+                    (myData.Count - xCount, myData.Count);
+            else
+                chart1.ChartAreas["draw"].AxisX.ScaleView.Zoom(0, xCount);
+
+  
+        }
+
+        /*
+        private void DBInsert(SensorData data)
+        {
+            string sql = string.Format(
+                "INSERT INTO SenSorTable(Date, Time, Value) Valus{'{0}','{1}',{2})",
+                data.Date, data.Time, data.Value);
+
+            try
+            {
+                SqlConnection conn = new SqlConnection(connString);
+                SqlCommand comm = new SqlCommand(sql, conn);
+                {
+                    conn.Open();
+                    comm.ExecuteNonQuery();
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+        }
+        */
+
+        private void btnDisConnect_Click(object sender, EventArgs e)
+        {
+            sPort.Close();
+            btnConnect.Enabled = true;
+            btnDisConnect.Enabled = false;
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            sPort.Open();
+            btnDisConnect.Enabled = true;
+            btnConnect.Enabled = false;
+        }
+    }
+}
